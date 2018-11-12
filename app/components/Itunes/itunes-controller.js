944d3e5a593dc1c56547cbd497b59b2b6a7799a3
Marcus Beall <marcbeall@gmail.com>
2018-11-12 14:08:24 -0700

    eh

--- app/components/Itunes/itunes-controller.js
+++ app/components/Itunes/itunes-controller.js
@@ -7,9 +7,22 @@ const itunesService = new ItunesService()
 function drawSongs(results) {
   console.log(results)
   //YOUR CODING STARTS HERE
-
-
-
+  let template = ''
+  let songs = itunesService.getMusicByArtist(results)
+  for (let i = 0; i < results.length; i++) {
+    let music = results[i];
+    template += `
+    <div class="col">
+                    <div class="card text-white bg-primary mb-3" style="width: 250px;">
+                        <img src="${music.albumArt}" alt="album"/>
+                        <p>${music.price}</p>
+                        <h5>${music.title}</h5>
+                        <h5>Artist: ${music.artist}</h5>
+                    </div>
+                </div>
+`
+  }
+  document.getElementById("songs").innerHTML = template
 }
 
 
